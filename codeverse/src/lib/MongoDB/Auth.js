0e391ef0f307f6cc93976b27c38f015622623625
Gopal562004 <gopalgawas2004@gmail.com>
2024-09-24 22:07:02 +0530

    Updated all files to latest version

--- codeverse/src/lib/MongoDB/Auth.js
+++ codeverse/src/lib/MongoDB/Auth.js
@@ -1,3 +1,45 @@
+// import axios from "axios";
+
+// export const CreateUserAccountMutation = async (user) => {
+//   try {
+//     const response = await axios.post(
+//       "http://localhost:5000/user/sign-up",
+//       user
+//     );
+//     return response.data;
+//   } catch (error) {
+//     throw new Error(error.response.data.message);
+//   }
+// };
+
+// export const SignInAccountMutation = async (user) => {
+//   try {
+//     const response = await axios.post(
+//       "http://localhost:5000/user/sign-in",
+//       user
+//     );
+//     return response.data;
+//   } catch (error) {
+//     throw new Error(error.response.data.message);
+//   }
+// };
+// export const SignOutAccountMutation = async () => {
+//   try {
+//     await axios.post("http://localhost:5000/user/sign-out");
+//     // Optionally clear any local storage or client-side state
+//   } catch (error) {
+//     throw new Error(error.response.data.message);
+//   }
+// };
+// export async function getCurrentUser() {
+//   try {
+//     const response = await axios.get("/user/current"); // Adjust endpoint as per your server setup
+//     return response.data.user; // Assuming your server returns user data in 'user' field
+//   } catch (error) {
+//     console.error("Error fetching current user:", error);
+//     throw error;
+//   }
+// }
 import axios from "axios";
 
 export const CreateUserAccountMutation = async (user) => {
@@ -23,6 +65,7 @@ export const SignInAccountMutation = async (user) => {
     throw new Error(error.response.data.message);
   }
 };
+
 export const SignOutAccountMutation = async () => {
   try {
     await axios.post("http://localhost:5000/user/sign-out");
@@ -31,7 +74,8 @@ export const SignOutAccountMutation = async () => {
     throw new Error(error.response.data.message);
   }
 };
-export async function getCurrentUser() {
+
+export const getCurrentUser = async () => {
   try {
     const response = await axios.get("/user/current"); // Adjust endpoint as per your server setup
     return response.data.user; // Assuming your server returns user data in 'user' field
@@ -39,4 +83,4 @@ export async function getCurrentUser() {
     console.error("Error fetching current user:", error);
     throw error;
   }
-}
+};
--- codeverse/src/lib/MongoDB/showpost.js
+++ codeverse/src/lib/MongoDB/showpost.js
@@ -1,3 +1,14 @@
+// import axios from "axios";
+
+// // showpost.js
+// const fetchPosts = async ({ pageParam = 1 }) => {
+//   const response = await axios.get(
+//     `http://localhost:5000/posts/posts?page=${pageParam}&limit=4`
+//   );
+//   return response.data;
+// };
+
+// export default fetchPosts;
 import axios from "axios";
 
 // showpost.js
